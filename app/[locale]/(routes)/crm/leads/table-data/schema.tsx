e42c8472b78abe623be53ddce0b0a940bc89beb2
Dekan Brown <dekanbro@gmail.com>
2026-06-19 06:58:03 -0600

    fix: allow short lead names in table

--- app/[locale]/(routes)/crm/leads/table-data/schema.tsx
+++ app/[locale]/(routes)/crm/leads/table-data/schema.tsx
@@ -3,12 +3,11 @@ import { z } from "zod";
 // We're keeping a simple non-relational schema here.
 // IRL, you will have a schema for your data models.
 export const leadSchema = z.object({
-  //TODO: fix all the types and nullable
   id: z.string(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
+  createdAt: z.date().nullable(),
+  updatedAt: z.date().nullable(),
   firstName: z.string().optional().nullable(),
-  lastName: z.string().min(3).max(30).nonempty(),
+  lastName: z.string().min(1),
 });
 
 export type Lead = z.infer<typeof leadSchema>;
